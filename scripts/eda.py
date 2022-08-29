ef5ee6e4c7b3b96cf24fe338ad00bd16dda080e7
Josias-Ounsinli <josias.ouns.djossou@gmail.com>
2022-08-29 18:28:48 +0100

    Performing data exploration

--- scripts/eda.py
+++ scripts/eda.py
@@ -3,6 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+def load_data(path: str):
+    df = pd.read_csv(path)
+    return df
+
 def data_shape(df):
     print(f" There are {df.shape[0]} rows and {df.shape[1]} columns")
 
@@ -26,6 +30,11 @@ def percent_missing(df):
     # Calculate percentage of missing values
     print("The dataset contains", round(((totalMissing/totalCells) * 100), 2), "%", "missing values.")
 
+def top_values(df:pd.DataFrame, column:str, top:int) -> None:
+    values = df[column].value_counts()
+    topdf = df.loc[df[column].isin(list(values[:top].index))]
+    return topdf
+
 def plot_count(df:pd.DataFrame, column:str) -> None:
     plt.figure(figsize=(20,10))
     fig = sns.countplot(data = df, x = column)
@@ -37,7 +46,6 @@ def plot_count(df:pd.DataFrame, column:str) -> None:
     for i in range(len(df[column].value_counts())):
         fig.text(i, (df[column].value_counts()[i])/2, str(df[column].value_counts()[i]), fontdict = dict(color = 'black', fontsize = 30), horizontalalignment = 'center')
 
-
 def multi_plot_count(df:pd.DataFrame, column:str, hue: str) -> None:
     plt.figure(figsize=(20,10))
     fig = sns.countplot(data = df, x = column, hue=hue)
@@ -46,12 +54,10 @@ def multi_plot_count(df:pd.DataFrame, column:str, hue: str) -> None:
     plt.yticks(fontsize=20)
     plt.xlabel(column, fontsize=20)
     plt.ylabel('Absolute frequencies', fontsize=20)
-    
-    for bars in fig.containers:
-        for p in fig.patches:
-            width = p.get_width()
-            height = p.get_height()
-            x, y = p.get_xy()
-            fig.annotate(f'{(height)}', (x + width/2, y + height*1.02), ha='center', size = 15)
 
+    for p in fig.patches:
+        width = p.get_width()
+        height = p.get_height()
+        x, y = p.get_xy()
+        fig.annotate(f'{(height)}', (x + width/2, y + height*1.02), ha='center', size = 15)
 
