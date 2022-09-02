1cd1bfbf169081ba9c0f6637f07de79af57df60c
kebishaa <kbkibatu@gmail.com>
2022-09-02 15:54:35 +0300

    added plot scatter data frame

--- scripts/data_visualization.py
+++ scripts/data_visualization.py
@@ -22,3 +22,29 @@ class exploration:
                     vmax=1, fmt='.2f', linewidths=.7, cbar=cbar)
         plt.title(title, size=20, fontweight='bold')
         plt.show()
+    def plot_heatmap_from_correlation(correlation, title: str):
+        '''
+        heatmap: Plot rectangular data as a color-encoded matrix and correlation matrix.
+        title: Title of the plot
+        correlation: correlation matrix
+        '''
+        plt.figure(figsize=(14, 9))
+        sns.heatmap(correlation)
+        plt.title(title, size=18, fontweight='bold')
+        plt.show()
+
+    def plot_scatter(df: pd.DataFrame, x_col: str, y_col: str, title: str, hue: str, style: str) -> None:
+        '''
+        # scatter: Plot data as a scatter plot.
+        # df: dataframe to be plotted
+        # x_col: x-axis column
+        # y_col: y-axis column
+        # title: Title of the plot
+        # hue: hue column
+        '''
+        plt.figure(figsize=(12, 7))
+        sns.scatterplot(data=df, x=x_col, y=y_col, hue=hue, style=style)
+        plt.title(title, size=20)
+        plt.xticks(fontsize=14)
+        plt.yticks(fontsize=14)
+        plt.show()
